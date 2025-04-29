8a4b59907daa84bfa8dd9412057303041ca4b7b7
YassineSalihi <y.salihi7457@uca.ac.ma>
2025-04-30 00:54:19 +0100

    server in ex1, mistake made

--- src/ex1/Client.java
+++ src/ex1/Client.java
@@ -17,32 +17,5 @@ import java.net.Socket;
  */
 public class Client {
     
-    public static void main(String[] args) {
-        
-        try (ServerSocket serverSocket = new ServerSocket(1234)) {
-            System.out.println("Serveur démarré, en attente d'une connexion...");
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Client connecté.");
-            
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
-                   
-            String message;
-            while((message = in.readLine()) != null) {  
-                if(message.equals("exit")){
-                    System.out.println("byyyyye!");
-                    break;
-                }
-                
-             System.out.println("[Client] : " + message);
-             out.println("Message reçu : " + message);
-                
-            }
-            
-            System.out.println("connexion fermée");
-                    
-        } catch (Exception e) {
-            System.out.println("Erreur serveur : " + e.getMessage());
-        }
-    }
+
 }
--- src/ex1/Server.java
+++ src/ex1/Server.java
@@ -5,10 +5,43 @@
  */
 package ex1;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author yassinesalihi
  */
 public class Server {
-    
+        public static void main(String[] args) {
+        
+        try (ServerSocket serverSocket = new ServerSocket(1234)) {
+            System.out.println("Serveur démarré, en attente d'une connexion...");
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connecté.");
+            
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+                   
+            String message;
+            while((message = in.readLine()) != null) {  
+                if(message.equals("exit")){
+                    System.out.println("byyyyye!");
+                    break;
+                }
+                
+             System.out.println("[Client] : " + message);
+             out.println("Message reçu : " + message);
+                
+            }
+            
+            System.out.println("connexion fermée");
+                    
+        } catch (Exception e) {
+            System.out.println("Erreur serveur : " + e.getMessage());
+        }
+    }
 }
